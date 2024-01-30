1531375fcf923f7889cf36c3ad2cf5001f70d12c
jcarin <me@jcarin.com>
2024-01-30 13:03:12 -0700

    make simple_example what I want

--- examples/simple_example.cc
+++ examples/simple_example.cc
@@ -1,9 +1,6 @@
-// Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
-//  This source code is licensed under both the GPLv2 (found in the
-//  COPYING file in the root directory) and Apache 2.0 License
-//  (found in the LICENSE.Apache file in the root directory).
-
 #include <cstdio>
+#include <cstdlib>  // for rand() and srand()
+#include <ctime>    // for time()
 #include <string>
 
 #include "rocksdb/db.h"
@@ -18,76 +15,37 @@ using ROCKSDB_NAMESPACE::Status;
 using ROCKSDB_NAMESPACE::WriteBatch;
 using ROCKSDB_NAMESPACE::WriteOptions;
 
-#if defined(OS_WIN)
-std::string kDBPath = "C:\\Windows\\TEMP\\rocksdb_simple_example";
-#else
-std::string kDBPath = "/tmp/rocksdb_simple_example";
-#endif
+std::string kDBPath = "/users/jcarin/rocksdb-database";
+int num_entries = 1000000;
 
-int main() {
+
+int main(int argc, char* argv[]) {
+  if (argc > 1)
+    num_entries = std::atoi(argv[1]);
+
+  srand(time(0));
+
   DB* db;
   Options options;
-  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
   options.IncreaseParallelism();
   options.OptimizeLevelStyleCompaction();
-  // create the DB if it's not already present
   options.create_if_missing = true;
 
-  // open DB
+  options.write_buffer_size = 4 * 1024 * 1024;
+  options.max_write_buffer_number = 2;
+  options.target_file_size_base = 2 * 1024 * 1024;
+
   Status s = DB::Open(options, kDBPath, &db);
   assert(s.ok());
 
-  // Put key-value
-  s = db->Put(WriteOptions(), "key1", "value");
-  assert(s.ok());
-  std::string value;
-  // get value
-  s = db->Get(ReadOptions(), "key1", &value);
-  assert(s.ok());
-  assert(value == "value");
+  for (int i = 0; i < num_entries; i++) {
+    int random_number = rand();  // Generate a random number
+    std::string key = "key" + std::to_string(random_number);  // Generate a key
+    std::string value = "value" + std::to_string(i);  // Generate a value
 
-  // atomically apply a set of updates
-  {
-    WriteBatch batch;
-    batch.Delete("key1");
-    batch.Put("key2", value);
-    s = db->Write(WriteOptions(), &batch);
+    s = db->Put(WriteOptions(), key, value);
+    assert(s.ok());
   }
-
-  s = db->Get(ReadOptions(), "key1", &value);
-  assert(s.IsNotFound());
-
-  db->Get(ReadOptions(), "key2", &value);
-  assert(value == "value");
-
-  {
-    PinnableSlice pinnable_val;
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-    assert(pinnable_val == "value");
-  }
-
-  {
-    std::string string_val;
-    // If it cannot pin the value, it copies the value to its internal buffer.
-    // The intenral buffer could be set during construction.
-    PinnableSlice pinnable_val(&string_val);
-    db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-    assert(pinnable_val == "value");
-    // If the value is not pinned, the internal buffer must have the value.
-    assert(pinnable_val.IsPinned() || string_val == "value");
-  }
-
-  PinnableSlice pinnable_val;
-  s = db->Get(ReadOptions(), db->DefaultColumnFamily(), "key1", &pinnable_val);
-  assert(s.IsNotFound());
-  // Reset PinnableSlice after each use and before each reuse
-  pinnable_val.Reset();
-  db->Get(ReadOptions(), db->DefaultColumnFamily(), "key2", &pinnable_val);
-  assert(pinnable_val == "value");
-  pinnable_val.Reset();
-  // The Slice pointed by pinnable_val is not valid after this point
-
-  delete db;
 
   return 0;
 }
